ec66d5ab4b68bf611781d75f330dc476a1a53d47
vladsuper5555 <butnaruvlad2003@yahoo.com>
2024-05-18 14:40:40 +0300

    implemented all databases and automated script for loading them

--- initDatabase.js
+++ initDatabase.js
@@ -1,6 +1,26 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const fs = require("fs");
+const path = require("path");
 
-var con = mysql.createConnection({
+// Database configurations
+const databases = [
+  { databaseName: "technologies", databaseConfigName: "technologies.json" },
+  { databaseName: "languages", databaseConfigName: "languagesDatabase.json" },
+  { databaseName: "license", databaseConfigName: "licenseDatabase.json" },
+  { databaseName: "liveCoding", databaseConfigName: "liveCodingDatabase.json" },
+  { databaseName: "platforms", databaseConfigName: "platformsDatabase.json" },
+  { databaseName: "purpose", databaseConfigName: "purposeDatabase.json" },
+  { databaseName: "realtimeCollaboration", databaseConfigName: "realtimeCollaborationDatabase.json" },
+  { databaseName: "rendering", databaseConfigName: "renderingDatabase.json" },
+  { databaseName: "type", databaseConfigName: "typeDatabase.json" }
+];
+
+// Function to read JSON files
+const readJsonFile = (filePath) => {
+  return JSON.parse(fs.readFileSync(filePath, "utf8"));
+};
+
+const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
@@ -25,7 +45,7 @@ con.connect(function (err) {
       con.end(function (err) {
         if (err) throw err;
 
-        var conDB = mysql.createConnection({
+        const conDB = mysql.createConnection({
           host: "localhost",
           user: "root",
           password: "",
@@ -34,10 +54,10 @@ con.connect(function (err) {
 
         conDB.connect(async function (err) {
           if (err) throw err;
-          console.log("Connected to BookingIasi!");
+          console.log("Connected to ReFI!");
 
-          let createTableSql = `CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
+          let createUserTableSql = `CREATE TABLE IF NOT EXISTS users (
+            id INT PRIMARY KEY,
             email VARCHAR(255) NOT NULL,
             password TEXT NOT NULL,
             darktheme BOOLEAN,
@@ -46,27 +66,70 @@ con.connect(function (err) {
           );`;
           
           await new Promise((res) => {
-            conDB.query(createTableSql, function (err, result) {
+            conDB.query(createUserTableSql, function (err, result) {
+              if (err) throw err;
               res(result);
             });
           });
-          
 
+          // Process each database configuration
+          for (const dbConfig of databases) {
+            const data = readJsonFile(path.join(__dirname, "databaseManagement", dbConfig.databaseConfigName));
 
-        //   let insertSql = `INSERT INTO users 
-        //   (email, givenName, familyName, password, username, birthDate, height, weight, gender, needsSpecialAssistance, userAgreedToFetchData, activityIndex) 
-        //   VALUES 
-        //   (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+            if (data.length > 0) {
+              // Create table based on the first object keys
+              const columns = Object.keys(data[0]).map(key => `${key} ${key === 'id' ? 'INT' : 'VARCHAR(255)'}`).join(", ");
+              const createTableSql = `CREATE TABLE IF NOT EXISTS ${dbConfig.databaseName} (${columns});`;
 
-        //   userData.forEach((user) => {
-        //     new Promise((res) => {
-        //       conDB.query(insertSql, user, function (err, result) {
-        //         if (err) throw err;
-        //         console.log("User record inserted:", result.insertId);
-        //         res(result);
-        //       });
-        //     });
-        //   });
+              await new Promise((res) => {
+                conDB.query(createTableSql, function (err, result) {
+                  if (err) throw err;
+                  res(result);
+                });
+              });
+
+              // Insert data into the table
+              for (const record of data) {
+                // Find keys that contain arrays
+                const arrayKeys = Object.keys(record).filter(key => Array.isArray(record[key]));
+                
+                // Generate all records to be inserted
+                let recordsToInsert = [record];
+                
+                for (const key of arrayKeys) {
+                  const newRecords = [];
+                  
+                  for (const value of record[key]) {
+                    for (const rec of recordsToInsert) {
+                      const newRecord = { ...rec, [key]: value };
+                      newRecords.push(newRecord);
+                    }
+                  }
+                  
+                  recordsToInsert = newRecords;
+                }
+
+                // Insert all records into the table
+                const insertSql = `INSERT INTO ${dbConfig.databaseName} (${Object.keys(record).join(", ")}) VALUES ?`;
+                const values = recordsToInsert.map(Object.values);
+                if (values.length == 0)
+                  continue;
+
+                await new Promise((res) => {
+                  conDB.query(insertSql, [values], function (err, result) {
+                    if (err) throw err;
+                    console.log(`${dbConfig.databaseName} records inserted: ${result.affectedRows}`);
+                    res(result);
+                  });
+                });
+              }
+            }
+          }
+
+          conDB.end(function (err) {
+            if (err) throw err;
+            console.log("All databases loaded and connection closed.");
+          });
         });
       });
     });
