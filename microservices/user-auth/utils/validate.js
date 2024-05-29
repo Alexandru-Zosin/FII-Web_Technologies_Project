0845df622aabbc21d5d3d16b26294c3074af0b29
Alexandru <alex.zosin@yahoo.com>
2024-05-29 20:36:23 +0300

    fixed bug in email verification regex

--- microservices/user-auth/utils/validate.js
+++ microservices/user-auth/utils/validate.js
@@ -1,7 +1,7 @@
 const validateEmail = (email) => {
     if (!email)
         return false;
-    const pattern = /^[a-zA-Z0-9._%+-]{2,30}@[a-zA-Z0-9.-]{2,30}.[a-zA-Z]{2,10}$/;
+    const pattern = /^[a-zA-Z0-9._%+-]{2,30}@[a-zA-Z0-9.-]{2,30}\.[a-zA-Z]{2,10}$/;
     return pattern.test(email);
 };
 
